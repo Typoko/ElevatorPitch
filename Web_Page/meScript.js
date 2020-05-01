0a6ec18428511b54ba0822dff833eb8008634dd8
Timo Raivonen <timoraivonen@gmail.com>
2020-05-01 10:26:25 +0300

    Tekstin syöttöä ja pohjan luomista tekstin syötölle eri kielillä.

--- Web_Page/meScript.js
+++ Web_Page/meScript.js
@@ -1,6 +1,10 @@
 var tabPanels=document.querySelectorAll(".info .tabs .tab");
 var tabProjInfo=document.querySelectorAll(".projektiInfo");
-var listProj=document.querySelectorAll(".projekti");
+var listProj=document.querySelectorAll(".projektiNimi");
+var listProjNuol=document.querySelectorAll(".projektiNuoli");
+var listProjCanvas=document.querySelectorAll(".arrowCanvas");
+
+
 
 function showTab(buttonIndex){
     tabPanels.forEach(function(node){
@@ -13,12 +17,42 @@ function openInfo(projIndex){
     listProj.forEach(function(proj){
         proj.style.backgroundColor="#f5faff";
     });
+    listProjNuol.forEach(function(proj){
+        proj.style.backgroundColor="#f5faff";
+    });
     listProj[projIndex].style.backgroundColor="#d5eaff";
+    listProjNuol[projIndex].style.backgroundColor="#d5eaff";
+
 
     tabProjInfo.forEach(function(disp){
         disp.style.display="none";
     });
     tabProjInfo[projIndex].style.display="block";
+
+    rotateArrow(projIndex);
+}
+
+function drawArrows(){
+    listProjCanvas.forEach(function(canv){
+        var ctx = canv.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(5, 0);
+        ctx.lineTo(10, 9);
+        ctx.lineTo(0, 9);
+        ctx.closePath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#666666';
+        ctx.stroke();
+        ctx.fillStyle = "#B5CAFF";
+        ctx.fill();        
+    });    
+}
+
+function rotateArrow(arrowIndex){
+    listProjCanvas.forEach(function(canv){
+        canv.style.transform = "rotate(0deg)";
+    });
+    listProjCanvas[arrowIndex].style.transform = "rotate(90deg)";
 }
 
 
@@ -26,4 +60,7 @@ showTab(0);
 tabProjInfo.forEach(function(disp){
     disp.style.display="none";
 });
+drawArrows();
 openInfo(0);
+
+
---
+++ Web_Page/meText.js
@@ -0,0 +1,29 @@
+var tab1Div = document.getElementById("tab1");
+
+tab1Div.innerHTML +="<h1>Kuka olen?</h1> \
+<p>Hei, olen Timo Raivonen, tietotekniikan diplomi-insinööri. Sain viime joulukuussa opintoni valmiiksi ja olen nyt palaamassa takaisin työelämän pariin.</p> \
+<h1>Mitä osaan?</h1> \
+<p>Olen aikaisemmin toiminut ohjelmistosuunnittelijana laajassa projekteissa useamman vuoden ajan. Yhden koodarin tiiminä, työtehtäviini kuului ohjelmiston hallinnointi ja ohjelmointi C#, VB6 ja VBA kielillä. Kyseisten kielien lisäksi minulla on pintakosketusta moniin muihin ympäristöihin koulu- ja harrasteprojektien kautta.</p> \
+<h1>Mitä teen ja harrastan?</h1> \
+<p>Pidän ongelmien ratkaisemisesta ja en kaihda haasteita. Ohjelmoinnin lisäksi olen pitkään harrastanut pelejä niin tietokoneella kuin pöydän ääressä. Sen sijaan, että yritän löytää parhaat ratkaisut netistä, koitan miettiä pääni puhki löyttäkseni luovia ja tehokkaita vastauksia eteen tuleviin ongelmiin.</p> \
+<p>Rentouttavampana harrasteena käyn myös kävelyillä ja salilla, muokkailen kuvia ja kitaran soiton alkeita.</p>";
+
+//Nettisivut
+var projinfo1 = document.getElementById("pi1_text");
+
+projinfo1.innerHTML +="Testaus tekstille.";
+
+//Demoprojekti
+var projinfo2 = document.getElementById("pi2_text");
+
+projinfo2.innerHTML +="Testaus tekstille.";
+
+//Kuvamuokkaus
+var projinfo3 = document.getElementById("pi3_text");
+
+projinfo3.innerHTML +="Testaus tekstille.";
+
+//Peliprojekti
+var projinfo4 = document.getElementById("pi4_text");
+
+projinfo4.innerHTML +="Testaus tekstille.";
